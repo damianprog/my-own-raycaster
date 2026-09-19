7278cacb38a5904eb7ec22396490533c16d2d112
damianprog <damianpukszta@gmail.com>
2026-09-19 12:35:22 +0200

    Right angled triangle

--- src/point.js
+++ src/point.js
@@ -12,9 +12,16 @@ export default class Point {
     ctx.moveTo(this.position.x, this.position.y);
     ctx.lineTo(this.position.x, this.game.player.position.y);
     ctx.stroke();
+
     ctx.beginPath();
     ctx.moveTo(this.position.x, this.game.player.position.y);
     ctx.lineTo(this.game.player.position.x, this.game.player.position.y);
+    ctx.stroke();
+
+    ctx.strokeStyle = "#FF0000";
+    ctx.beginPath();
+    ctx.moveTo(this.game.player.position.x, this.game.player.position.y);
+    ctx.lineTo(this.position.x, this.position.y);
     ctx.stroke();
 
     ctx.restore();
